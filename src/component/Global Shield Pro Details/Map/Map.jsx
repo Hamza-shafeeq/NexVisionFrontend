140c97310c406f0ef05cfc12fbe6bb5a879ace99
SaadAhmed76 <saadahmed60610@gmail.com>
2023-11-28 04:18:23 +0500

    map buttons add

--- src/component/Global Shield Pro Details/Map/Map.jsx
+++ src/component/Global Shield Pro Details/Map/Map.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Icon } from "@iconify/react";
-
+import { BsFilterSquareFill } from "react-icons/bs";
 import Sidebar from "../../sidebar/sidebar";
 import { MapStyle } from "./MapStyle";
-// import locationIcon from '@iconify/icons-mdi/map-marker'
-
+import { IoSearchSharp } from "react-icons/io5";
+import { HiPlus } from "react-icons/hi";
 const LocationPin = ({ text }) => (
   <div className="pin">
     <Icon icon={Icon} className="pin-icon" />
@@ -15,12 +15,29 @@ const LocationPin = ({ text }) => (
 
 export default function Map(props) {
   return (
-    // <PhoneLookupDetailsStyledled>
     <MapStyle>
       <Sidebar>
-        <div className="map">
-          {/* <h2 className="map-h2">Come Visit Us At Our Campus</h2> */}
+        <div className="Box-Section">
+          <div className="FormBox">
+            <BsFilterSquareFill className="FormIcon" />
+            <span className="Detail-title1">Filter</span>
 
+            <IoSearchSharp className="FormIcon" />
+            <input className="NameBox" type="Email" placeholder="Email" />
+            <div className="Title-box">
+              <span className="Detail-title">Details</span>
+              <span className="Plus-Logo-Side">
+                <HiPlus className="FormIcon" />
+              </span>
+            </div>
+          </div>
+          <div class="button-container">
+            <button className="Next-Button">Map</button>
+
+            <button className="Next-Button">Satellite</button>
+          </div>
+        </div>
+        <div className="map">
           <div className="google-map">
             <GoogleMapReact
               bootstrapURLKeys={{ key: "" }}
