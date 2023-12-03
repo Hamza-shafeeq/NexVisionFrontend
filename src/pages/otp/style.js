74523a4136e69a346c2279a9c8fc598fe85115e5
SaadAhmed76 <saadahmed60610@gmail.com>
2023-12-04 03:50:49 +0500

    otp sign up repsonsive

--- src/pages/otp/style.js
+++ src/pages/otp/style.js
@@ -225,4 +225,592 @@ export const OtppageStyled = styled.div`
     font-size: 17px;
     font-weight: 500;
   }
+  @media all and (max-width: 1240px) {
+    .Signup-Background-Adjustment {
+      gap: 0px;
+    }
+  }
+  @media all and (max-width: 1110px) {
+    .Signup-Background-Adjustment {
+      gap: 0px;
+    }
+    .Sign-Logo {
+      width: 450px;
+      height: 500px;
+    }
+    .Sign-UP-Button {
+      height: 45px;
+      width: 250px;
+      border: none;
+      border-radius: 10px;
+      background-color: #591ddd;
+      color: white;
+      font-size: 17px;
+
+      font-weight: 400;
+    }
+    .NameBox {
+      width: 210px;
+      height: 37px;
+      font-size: 17px;
+      border: none;
+      outline: none;
+      color: #5d646e;
+    }
+    .Welcome-Text {
+      font-size: 17px;
+
+      font-weight: 600;
+    }
+    .Contact {
+      font-size: 20px;
+      color: #591ddd;
+      margin-top: 10px;
+    }
+    .FormBox {
+      border: 2px #916ae8 solid;
+      padding: 4px;
+      border-radius: 9px;
+      cursor: pointer;
+      display: flex;
+      background-color: white;
+      align-items: center;
+      width: 290px;
+    }
+    .ContactFormSide {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      padding-right: 10px;
+      margin-top: 20px;
+      gap: 30px;
+    }
+    .Nav-Logo {
+      width: 120px;
+    }
+    .nav-Text {
+      font-size: 11px;
+      gap: 3px;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+  }
+  @media all and (max-width: 870px) {
+    .Sign-Logo {
+      width: 0px;
+    }
+    .Signup-Background-Adjustment {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+      align-items: center;
+    }
+    .Sign-UP-Button {
+      height: 45px;
+      width: 250px;
+      border: none;
+      border-radius: 10px;
+      background-color: #591ddd;
+      color: white;
+      font-size: 17px;
+
+      font-weight: 400;
+    }
+    .NameBox {
+      width: 260px;
+      height: 37px;
+      font-size: 17px;
+      border: none;
+      outline: none;
+      color: #5d646e;
+    }
+    .Welcome-Text {
+      font-size: 17px;
+
+      font-weight: 600;
+    }
+    .Contact {
+      font-size: 20px;
+      color: #591ddd;
+      margin-top: 10px;
+    }
+    .FormBox {
+      border: 2px #916ae8 solid;
+      padding: 4px;
+      border-radius: 9px;
+      cursor: pointer;
+      display: flex;
+      background-color: white;
+      align-items: center;
+      width: 280px;
+    }
+    .ContactFormSide {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+
+      margin-top: 20px;
+      gap: 30px;
+    }
+    .Nav-Logo {
+      width: 120px;
+    }
+    .nav-Text {
+      font-size: 10px;
+      gap: 3px;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+    .Signup-Navbar-1 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 20px;
+      border-radius: 20px;
+      height: 60px;
+      border: 2px solid #8154e4;
+      width: 96%;
+      background-color: #eae5f6;
+    }
+    .Sign-Button-Section {
+      display: flex;
+      width: 100%;
+    }
+  }
+  @media all and (max-width: 627px) {
+    .Sign-Logo {
+      width: 0px;
+    }
+    .Signup-Background-Adjustment {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+      align-items: center;
+    }
+    .Sign-UP-Button {
+      height: 45px;
+      width: 250px;
+      border: none;
+      border-radius: 10px;
+      background-color: #591ddd;
+      color: white;
+      font-size: 17px;
+
+      font-weight: 400;
+    }
+    .nav-Text-Section {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 2px;
+    }
+    .Footer-CopyRight {
+      font-size: 13px;
+      font-weight: 700;
+    }
+    .Footer-Text {
+      font-size: 13px;
+      font-weight: 500;
+    }
+    .Log-text-Section {
+      display: flex;
+      width: 100%;
+      justify-content: flex-start;
+    }
+    .log-Text {
+      color: #8b97a8;
+      font-size: 14px;
+    }
+    .NameBox {
+      width: 260px;
+      height: 37px;
+      font-size: 17px;
+      border: none;
+      outline: none;
+      color: #5d646e;
+    }
+    .Welcome-Text {
+      font-size: 17px;
+
+      font-weight: 600;
+    }
+    .Contact {
+      font-size: 20px;
+      color: #591ddd;
+      margin-top: 10px;
+    }
+    .FormBox {
+      border: 2px #916ae8 solid;
+      padding: 4px;
+      border-radius: 9px;
+      cursor: pointer;
+      display: flex;
+      background-color: white;
+      align-items: center;
+      width: 280px;
+    }
+    .Footer-Main-Section {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 100px;
+      gap: 10px;
+    }
+    .Footer-CopyRight {
+      font-size: 13px;
+      font-weight: 700;
+    }
+    .Footer-Text {
+      font-size: 13px;
+      font-weight: 500;
+    }
+    .ContactFormSide {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+
+      margin-top: 20px;
+      gap: 30px;
+    }
+    .Nav-Logo {
+      width: 100px;
+    }
+    .nav-Text {
+      font-size: 10px;
+
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+    .Signup-Navbar-1 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 20px;
+      border-radius: 20px;
+      height: 60px;
+      border: 2px solid #8154e4;
+      width: 98%;
+      background-color: #eae5f6;
+    }
+    .Sign-Button-Section {
+      display: flex;
+      width: 100%;
+    }
+  }
+  @media all and (max-width: 457px) {
+    .Sign-Logo {
+      width: 0px;
+    }
+    .Signup-Background-Adjustment {
+      display: flex;
+      width: 87%;
+      justify-content: center;
+      align-items: center;
+    }
+    .Sign-UP-Button {
+      height: 45px;
+      width: 250px;
+      border: none;
+      border-radius: 10px;
+      background-color: #591ddd;
+      color: white;
+      font-size: 17px;
+
+      font-weight: 400;
+    }
+    .nav-Text-Section {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 2px;
+    }
+    .Footer-CopyRight {
+      font-size: 13px;
+      font-weight: 700;
+    }
+    .Footer-Text {
+      font-size: 13px;
+      font-weight: 500;
+    }
+    .Log-text-Section {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+    }
+    .log-Text {
+      color: #8b97a8;
+      font-size: 14px;
+    }
+    .NameBox {
+      width: 260px;
+      height: 37px;
+      font-size: 17px;
+      border: none;
+      outline: none;
+      color: #5d646e;
+    }
+    .Welcome-Text {
+      font-size: 17px;
+
+      font-weight: 600;
+    }
+    .Contact {
+      font-size: 20px;
+      color: #591ddd;
+      margin-top: 10px;
+    }
+    .FormBox {
+      border: 2px #916ae8 solid;
+      padding: 4px;
+      border-radius: 9px;
+      cursor: pointer;
+      display: flex;
+      background-color: white;
+      align-items: center;
+      width: 280px;
+    }
+    .Welcome-Section {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      width: 100%;
+    }
+    .Footer-Main-Section {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      margin-top: 100px;
+      gap: 10px;
+    }
+    .Footer-CopyRight {
+      font-size: 13px;
+      font-weight: 700;
+    }
+    .Footer-Text {
+      font-size: 13px;
+      font-weight: 500;
+    }
+    .ContactFormSide {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+
+      margin-top: 20px;
+      gap: 30px;
+    }
+    .Nav-Logo {
+      width: 70px;
+    }
+    .nav-Text {
+      font-size: 9px;
+      white-space: break-spaces;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+    .Signup-Navbar-1 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 20px;
+      border-radius: 20px;
+      height: 60px;
+      border: 2px solid #8154e4;
+      width: 96%;
+      background-color: #eae5f6;
+    }
+    .Sign-Button-Section {
+      display: flex;
+      width: 70%;
+    }
+    .Sign-Form {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+      width: 370px;
+      justify-content: center;
+      align-items: center;
+    }
+    .Forget-Text-Section {
+      display: flex;
+      gap: 10px;
+      width: 80%;
+      align-items: center;
+    }
+    .Signup-Container {
+      display: flex;
+      width: 86%;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+  @media all and (max-width: 380px) {
+    .Sign-Logo {
+      width: 0px;
+    }
+    .Signup-Background-Adjustment {
+      display: flex;
+      width: 87%;
+      justify-content: center;
+      align-items: center;
+    }
+    .Sign-UP-Button {
+      height: 45px;
+      width: 250px;
+      border: none;
+      border-radius: 10px;
+      background-color: #591ddd;
+      color: white;
+      font-size: 17px;
+
+      font-weight: 400;
+    }
+    .nav-Text-Section {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 2px;
+    }
+    .Footer-CopyRight {
+      font-size: 13px;
+      font-weight: 700;
+    }
+    .Footer-Text {
+      font-size: 13px;
+      font-weight: 500;
+    }
+    .Log-text-Section {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+    }
+    .log-Text {
+      color: #8b97a8;
+      font-size: 14px;
+    }
+    .NameBox {
+      width: 260px;
+      height: 37px;
+      font-size: 17px;
+      border: none;
+      outline: none;
+      color: #5d646e;
+    }
+    .Welcome-Text {
+      font-size: 17px;
+
+      font-weight: 600;
+    }
+    .Contact {
+      font-size: 20px;
+      color: #591ddd;
+      margin-top: 10px;
+    }
+    .FormBox {
+      border: 2px #916ae8 solid;
+      padding: 4px;
+      border-radius: 9px;
+      cursor: pointer;
+      display: flex;
+      background-color: white;
+      align-items: center;
+      width: 255px;
+    }
+    .Welcome-Section {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      width: 78%;
+    }
+    .Form-Box-Text {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      justify-content: center;
+      width: 73%;
+    }
+
+    .Footer-Main-Section {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      margin-top: 100px;
+      gap: 10px;
+    }
+    .Footer-CopyRight {
+      font-size: 13px;
+      font-weight: 700;
+    }
+    .Footer-Text {
+      font-size: 13px;
+      font-weight: 500;
+    }
+    .ContactFormSide {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+
+      margin-top: 20px;
+      gap: 30px;
+    }
+    .Nav-Logo {
+      width: 70px;
+    }
+    .nav-Text {
+      font-size: 9px;
+      white-space: break-spaces;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+    .Nav-Icon {
+      width: 15px;
+    }
+    .Signup-Navbar-1 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 20px;
+      border-radius: 20px;
+      height: 60px;
+      border: 2px solid #8154e4;
+      width: 100%;
+      background-color: #eae5f6;
+    }
+    .Sign-Button-Section {
+      display: flex;
+      width: 70%;
+    }
+    .Sign-Form {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+      width: 300px;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .Forget-Text-Section {
+      display: flex;
+      gap: 10px;
+      width: 80%;
+      align-items: center;
+    }
+    .Signup-Container {
+      display: flex;
+      width: 88%;
+      justify-content: center;
+      align-items: center;
+    }
+  }
 `;
