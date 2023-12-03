8d8893c045e66d4f275b78f86c29d03046ab79ea
SaadAhmed76 <saadahmed60610@gmail.com>
2023-12-04 04:16:04 +0500

    card header 50% responsive

--- src/component/ArmorNet Pro/Phone Lookup/AnpHeaderStyle.js
+++ src/component/ArmorNet Pro/Phone Lookup/AnpHeaderStyle.js
@@ -1,88 +1,221 @@
 import styled from "styled-components";
 
 export const AnpHeaderStyle = styled.div`
-display: flex;
-justify-content: center;
-background-color: #F4F4F4;
-
-.anp-title {
   display: flex;
   justify-content: center;
-  align-items: center;
-}
+  background-color: #f4f4f4;
 
-.anp-header-container {
-  height: 120px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 75px;
-  width: 100%;
-  padding-left: 31px;
-  padding-right: 150px;
+  .anp-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-}
-.anp-cta {
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  gap: 15px;
-}
+  .anp-header-container {
+    height: 120px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 75px;
+    width: 100%;
+    padding-left: 31px;
+    padding-right: 150px;
+  }
+  .anp-cta {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    gap: 15px;
+  }
 
-.anp-title-text {
-  color: black;
-  font-weight: 700;
-  
-}
+  .anp-title-text {
+    color: black;
+    font-weight: 700;
+  }
 
-.anp-btn {
-  /* display: */
-  width: 110px;
-  height: 45px;
-  background-color: #591ddd;
-  color: white;
-  font-weight: 500;
-  border: none;
-  border-radius: 8px;
-  font-size: 14px;
-}
-.anp-title-btn {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* margin-top: 22px; */
-}
-.anp-search {
-  width: 315px;
-  height: 37px;
-  font-size: 13px;
-  border: none;
-  outline: none;
-  color: black;
-  background-color: #EDEAF4;
-}
-.anp-search-box {
-  display: flex;
-  align-items: center;
-  background-color: #EDEAF4;
+  .anp-btn {
+    /* display: */
+    width: 110px;
+    height: 45px;
+    background-color: #591ddd;
+    color: white;
+    font-weight: 500;
+    border: none;
+    border-radius: 8px;
+    font-size: 14px;
+  }
+  .anp-title-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* margin-top: 22px; */
+  }
+  .anp-search {
+    width: 315px;
+    height: 37px;
+    font-size: 13px;
+    border: none;
+    outline: none;
+    color: black;
+    background-color: #edeaf4;
+  }
+  .anp-search-box {
+    display: flex;
+    align-items: center;
+    background-color: #edeaf4;
 
-  /* border: 2px #591DDD solid; */
-  padding: 4px;
-  border-radius: 9px;
-  cursor: pointer;
-}
-.anp-icon {
-  width: 35px;
-  height: 25px;
-  color: #591DDD;
-}
+    /* border: 2px #591DDD solid; */
+    padding: 4px;
+    border-radius: 9px;
+    cursor: pointer;
+  }
+  .anp-icon {
+    width: 35px;
+    height: 25px;
+    color: #591ddd;
+  }
 
-.anp-title-img{
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  width:100px;
-  height:100px;
-}
+  .anp-title-img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100px;
+    height: 100px;
+  }
 
+  @media all and (max-width: 1240px) {
+    .anp-header-container {
+      height: 120px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      gap: 50px;
+      width: 100%;
+      padding-left: 20px;
+      padding-right: 20px;
+    }
+    .anp-cta {
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      gap: 10px;
+    }
+    .anp-search {
+      width: 242px;
+      height: 37px;
+      font-size: 13px;
+      border: none;
+      outline: none;
+      color: black;
+      background-color: #edeaf4;
+    }
+  }
+  @media all and (max-width: 1000px) {
+    .anp-header-container {
+      height: 120px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      gap: 0px;
+      width: 100%;
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+    .anp-cta {
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      gap: 6px;
+    }
+    .anp-title-text {
+      color: black;
+      font-size: 15px;
+      font-weight: 600;
+    }
+    .anp-title {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100px;
+    }
+    .anp-title-img {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 72px;
+    }
+    .anp-cta {
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      gap: 8px;
+    }
+    .anp-btn {
+      width: 92px;
+      height: 38px;
+      background-color: #591ddd;
+      color: white;
+      font-weight: 500;
+      border: none;
+      border-radius: 8px;
+      font-size: 14px;
+    }
+    .anp-search {
+      width: 184px;
+      height: 37px;
+      font-size: 13px;
+      border: none;
+      outline: none;
+      color: black;
+      background-color: #edeaf4;
+    }
+  }
+  @media all and (max-width: 750px) {
+    .anp-header-container {
+      height: 120px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      gap: 0px;
+      width: 100%;
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+    .anp-title-text {
+      color: black;
+      font-size: 15px;
+      font-weight: 600;
+    }
+    .anp-title-img {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 60px;
+    }
+    .anp-cta {
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      gap: 6px;
+    }
+    .anp-btn {
+      width: 80px;
+      height: 40px;
+      background-color: #591ddd;
+      color: white;
+      font-weight: 500;
+      border: none;
+      border-radius: 8px;
+      font-size: 14px;
+    }
+    .anp-search {
+      width: 115px;
+      height: 37px;
+      font-size: 13px;
+      border: none;
+      outline: none;
+      color: black;
+      background-color: #edeaf4;
+    }
+  }
 `;
