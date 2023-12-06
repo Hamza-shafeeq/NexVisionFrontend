e280603d7c021c383eb423b6fb5f5c1af0e1deee
SaadAhmed76 <saadahmed60610@gmail.com>
2023-12-06 22:19:19 +0500

    general form 90% complete

--- src/component/GeneralDataAdd/GeneraldataAdd.jsx
+++ src/component/GeneralDataAdd/GeneraldataAdd.jsx
@@ -1,13 +1,122 @@
 import React from "react";
 import { GeneralDataAddStyle } from "./style";
 import Sidebar from "../sidebar/sidebar";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { RiCalendarTodoLine } from "react-icons/ri";
 
 function GeneraldataAdd() {
   return (
     <>
       <Sidebar>
         <GeneralDataAddStyle>
-          <div>hello</div>
+          <div className="GeneralDataAdd-Main-Section">
+            <div className="GeneralDataAdd-Container">
+              <div className="GeneralAddBox-Header">
+                <div className="GeneralAdd-Title-Section">
+                  <div className="GeneralAdd-TextSection">
+                    <span className="GeneralAdd-Number1">1</span>
+                    <span className="GeneralAdd-Title1">General</span>
+                  </div>
+                  <MdOutlineKeyboardArrowRight className="Arrow-Icon" />
+                </div>
+                <div className="GeneralAdd-Title-Section">
+                  <div className="GeneralAdd-TextSection">
+                    <span className="GeneralAdd-Number">2</span>
+                    <span className="GeneralAdd-Title">Personal</span>
+                  </div>{" "}
+                  <MdOutlineKeyboardArrowRight className="Arrow-Icon" />
+                </div>
+                <div className="GeneralAdd-Title-Section">
+                  <div className="GeneralAdd-TextSection">
+                    <span className="GeneralAdd-Number">3</span>
+                    <span className="GeneralAdd-Title">User</span>
+                  </div>
+                  <MdOutlineKeyboardArrowRight className="Arrow-Icon" />
+                </div>
+                <div className="GeneralAdd-Title-Section">
+                  <div className="GeneralAdd-TextSection">
+                    <span className="GeneralAdd-Number">4</span>
+                    <span className="GeneralAdd-Title">Image Research</span>
+                  </div>
+                  <MdOutlineKeyboardArrowRight className="Arrow-Icon" />
+                </div>
+                <div className="GeneralAdd-Title-Section">
+                  <div className="GeneralAdd-TextSection">
+                    <span className="GeneralAdd-Number">5</span>
+                    <span className="GeneralAdd-Title">Social media</span>
+                  </div>
+                  <MdOutlineKeyboardArrowRight className="Arrow-Icon" />
+                </div>
+                <div className="GeneralAdd-Title-Section">
+                  <div className="GeneralAdd-TextSection">
+                    <span className="GeneralAdd-Number">6</span>
+                    <span className="GeneralAdd-Title">Source</span>
+                  </div>
+                  <MdOutlineKeyboardArrowRight className="Arrow-Icon" />
+                </div>
+                <div className="GeneralAdd-Title-Section">
+                  <div className="GeneralAdd-TextSection">
+                    <span className="GeneralAdd-Number">7</span>
+                    <span className="GeneralAdd-Title">Setting</span>
+                  </div>
+                  <MdOutlineKeyboardArrowRight className="Arrow-Icon" />
+                </div>
+              </div>
+              <div className="GeneralAdd-Header-Gapping">
+                <div className="General-Add-Header">
+                  <h1>Third Party Intelligence</h1>
+                  <span className="General-title">General </span>
+                </div>
+              </div>
+              <div className="Form-Gapping">
+                <div className="Forms-Setting">
+                  <div className="Form-Titles-Setting">
+                    <span className="Form-Titles">Case Id</span>
+                    <div className="FormBox">
+                      <input
+                        className="NameBox"
+                        type="Text"
+                        placeholder="Enter Id "
+                      />
+                    </div>
+                  </div>
+                  <div className="Form-Titles-Setting">
+                    <span className="Form-Titles">Case Date</span>
+                    <div className="FormBox">
+                      <input
+                        className="NameBox"
+                        type="Text"
+                        placeholder="MM/DD/YYYY"
+                      />
+                      <RiCalendarTodoLine className="EyeIcon" />
+                    </div>
+                  </div>
+                </div>
+                <div className="Forms-Setting">
+                  <div className="Form-Titles-Setting">
+                    <span className="Form-Titles">Case Description</span>
+                    <div className="FormBox">
+                      <input
+                        className="NameBox"
+                        type="Text"
+                        placeholder="Enter Description "
+                      />
+                    </div>
+                  </div>{" "}
+                  <div className="Form-Titles-Setting">
+                    <span className="Form-Titles">Investigator Name</span>
+                    <div className="FormBox">
+                      <input
+                        className="NameBox"
+                        type="Text"
+                        placeholder="Enter Name "
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </GeneralDataAddStyle>
       </Sidebar>
     </>
--- src/component/GeneralDataAdd/style.js
+++ src/component/GeneralDataAdd/style.js
@@ -2,4 +2,148 @@ import styled from "styled-components";
 
 export const GeneralDataAddStyle = styled.div`
   background-color: white;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  .GeneralDataAdd-Main-Section {
+    display: flex;
+    width: 100%;
+
+    flex-direction: column;
+  }
+  .GeneralAddBox-Header {
+    width: 90%;
+    margin-top: 10px;
+    display: flex;
+    height: 58px;
+    justify-content: center;
+    align-items: center;
+    gap: 14px;
+    border-bottom: 3px solid #7c7b7f;
+  }
+
+  .GeneralAdd-Title-Section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  .GeneralAdd-TextSection {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
+  .GeneralAdd-Number {
+    width: 28px;
+    font-size: 10px;
+    height: 25px;
+    font-weight: 700;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    background-color: #e7e7e7;
+  }
+  .GeneralAdd-Number1 {
+    width: 28px;
+    font-size: 10px;
+    height: 25px;
+    font-weight: 700;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    background-color: #633bbc;
+    color: white;
+  }
+
+  .GeneralAdd-Title {
+    display: flex;
+    font-size: 17px;
+    font-weight: 600;
+    color: black;
+  }
+  .General-Add-Header {
+    display: flex;
+    flex-direction: column;
+  }
+  .GeneralAdd-Title1 {
+    display: flex;
+    font-size: 17px;
+    font-weight: 600;
+    color: #633bbc;
+  }
+  .General-title {
+    font-size: 18px;
+    font-weight: 600;
+    border-bottom: 3px solid #7c7b7f;
+    width: 63px;
+    padding-bottom: 5px;
+  }
+  .NameBox {
+    width: 350px;
+    height: 37px;
+    font-size: 17px;
+    border: none;
+    outline: none;
+    background-color: #efeef2;
+  }
+  .FormBox {
+    border: 2px #e6ebf2 solid;
+    width: 380px;
+    padding: 4px;
+    background-color: #efeef2;
+    border-radius: 9px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+  }
+  .FormIcon {
+    width: 25px;
+    height: 17px;
+    color: #8b97a8;
+  }
+  .Form-Text {
+    display: flex;
+    flex-direction: column;
+  }
+  .Forms-Setting {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 90%;
+    gap: 10px;
+  }
+  .Form-Titles-Setting {
+    flex-direction: column;
+    display: flex;
+    gap: 5px;
+  }
+  .Form-Titles {
+    font-size: 18px;
+    font-weight: 400;
+  }
+  .Form-Gapping {
+    display: flex;
+    flex-direction: column;
+
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
+  }
+  .GeneralAdd-Header-Gapping {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 64%;
+    margin-bottom: 40px;
+  }
+  .GeneralDataAdd-Container {
+    display: flex;
+    width: 100%;
+
+    background-color: #faf8ff;
+    flex-direction: column;
+  }
 `;
